b11999b6c000e5937ee7377f63a0d4ce576a33c6
Mateus Valente <73913605+MateusSValente@users.noreply.github.com>
2026-09-12 18:29:10 -0300

    Add WebView activity for Cafe app

---
+++ cafe-android/app/src/main/java/com/cafedafamilia/app/MainActivity.java
@@ -0,0 +1,46 @@
+package com.cafedafamilia.app;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setStatusBarColor(Color.rgb(246, 241, 235));
+        window.setNavigationBarColor(Color.rgb(255, 253, 250));
+
+        webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(246, 241, 235));
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setTextZoom(100);
+
+        webView.setWebViewClient(new WebViewClient());
+        setContentView(webView);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
